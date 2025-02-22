edcaa76c6ccb5c1d27c3fd7697a10e8d099e091f
Igor Tarlinskiy <itarlinskiy@gmail.com>
2025-02-22 06:16:34 +0500

    CORS fix

--- sui/api/run.py
+++ sui/api/run.py
@@ -1,9 +1,12 @@
 import os
 from pathlib import Path
 
+import dotenv
 from loguru import logger
 from quart import Quart, send_from_directory
 from quart_cors import cors
+
+dotenv.load_dotenv()
 
 app = Quart(
     __name__,
@@ -12,7 +15,20 @@ app = Quart(
     template_folder=str(Path(os.getcwd()) / "sui" / "dist"),
 )
 
-app = cors(app, allow_origin=["http://127.0.0.1", "https://127.0.0.1"])
+app = cors(
+    app,
+    send_origin_wildcard=False,
+    allow_credentials=True,
+    allow_origin=[
+        f"http://0.0.0.0:{os.environ['SUI_PORT_ALLOW_ORIGIN']}",
+        f"http://localhost:{os.environ['SUI_PORT_ALLOW_ORIGIN']}",
+        f"http://127.0.0.1:{os.environ['SUI_PORT_ALLOW_ORIGIN']}",
+        f"https://0.0.0.0:{os.environ['SUI_PORT_ALLOW_ORIGIN']}",
+        f"https://localhost:{os.environ['SUI_PORT_ALLOW_ORIGIN']}",
+        f"https://127.0.0.1:{os.environ['SUI_PORT_ALLOW_ORIGIN']}",
+    ],
+    allow_headers=["*"],
+)
 
 logger.info(f"Using static folder path=[{str(Path(os.getcwd()) / 'sui' / 'dist')}]")
 logger.info(f"Using template folder path=[{str(Path(os.getcwd()) / 'sui' / 'dist')}]")
